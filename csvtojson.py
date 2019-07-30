b824fe9d7516fc657a50696b61d7a42609fbfe1b
elvinjgalarza <root@DESKTOP-6NPPO1K.localdomain>
2019-07-29 22:31:00 -0400

    created a csv to json converter

--- csvtojson.py
+++ csvtojson.py
@@ -1,15 +1,59 @@
 import csv
 
+file = open("wampus.txt", "w+")
+file.write("[{")
+
 with open('Austin_Affordable_Housing.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter = ',')
     print()
     print("Parsing data...")
     print()
     
-    #Put rows into a list
-    rows = list(reader)
-    
-    #row iterator
+    #Put excel into a list
+    excel = list(reader)
+
+    #excel[X][2] is for project name
+    #excel[X][6] is for zip codes
+    #excel[X][12] is for tenure types (i.e., rental)
+    #excel[X][56] is for location(lat,lon)
+    #excel iterator
     i = 0
-    for c in rows:
-        if row[i]
+    n = 0
+    for row in excel:
+        if excel[i][6] == "78,705":
+            if excel[i][12] == "Rental":
+                #ID
+                file.write('"ID":')
+                file.write(str(n))
+                n+=1
+                file.write(',')
+
+                #Name
+                file.write('"Name:"')
+                file.write(excel[i][2])
+                file.write(',')
+
+                #Address
+                file.write('"Address:"')
+                file.write(excel[i][5])
+                file.write(',')
+
+                #Zip Code
+                file.write("Zip Code: 78705,")
+
+                #Latitude
+                file.write('"lat:"')
+                file.write(excel[i][55])
+
+                #Longitude
+                file.write('"lng:"')
+                file.write(excel[i][54])
+                file.write("}")
+                if n > 0 and n < 66:
+                    file.write(",")
+
+                #file.write(excel[i][56]) 
+        i+=1
+
+    file.write("]")
+    file.close()
